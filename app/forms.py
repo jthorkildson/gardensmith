8f64dc42284a16e745a82892259995437a45cda4
jthorkildson <jtork33@gmail.com>
2026-05-13 22:21:05 -0600

    add services and contact page with form

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    phone = StringField('Phone')
+    message = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField('Send')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
+from flask import redirect, render_template, url_for
 from app import app
+from app.forms import ContactForm
 
 @app.route('/')
 @app.route('/index')
-@app.route('/services')
-@app.route('/contact')
 def index():
-    return "hello world"
+    return render_template('index.html', title='Home')
+
+@app.route('/services')
+def services():
+    return render_template('services.html', title='Services')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        # Process the form data (e.g., send an email, save to database)
+        return redirect(url_for('index'))
+    return render_template('contact.html', title='Contact Us', form=form)
